d030e073d1340579a6be0284b8f7754b30e28ec7
MK <alsrnr1103@naver.com>
2022-01-22 15:43:53 +0900

    implementation content modification

--- src/main/java/com/jojoldu/book/mk/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/mk/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.jojoldu.book.mk.springboot.web;
 
 import com.jojoldu.book.mk.springboot.service.posts.PostsService;
+import com.jojoldu.book.mk.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,4 +26,13 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
 }
